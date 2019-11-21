ee073da624903cfe4efd50337a837e710f2a7179
maickol <maickol_sm@outlook.com>
2019-11-21 01:01:29 -0500

    programa cambios

--- SimulacionVRWeb/Content/assets/scripts/Script_Programa.js
+++ SimulacionVRWeb/Content/assets/scripts/Script_Programa.js
@@ -33,9 +33,22 @@ $(document).ready(function () {
     $('.basic-clockpicker').clockpicker();
 
     CurrectSelecteditem('#li_programa');
+
     Load_Programa({
         FechaInicio: '2019-10-11',
         FechaFin: '2019-11-20'
+    });
+
+    $(".agregarCiudadano").on("click", function () {
+        ID = 0;
+        Load_Local();
+        Load_Simulacion();
+    });
+
+    $(document).on("click", ".btn_edit", function () {
+        ID = $(this).attr("data-id");
+        $("#listaprograma").hide();
+        $("#editarprograma").show({ direction: "right" }, 5000);
     });
     
 
@@ -72,12 +85,64 @@ function Load_Programa(data) {
                 resultTable += "<td>" + item.HoraInicio + "</td>";
                 resultTable += "<td>" + item.HoraFin + "</td>";
                 resultTable += "<td><center>";
-                resultTable += "<button data-id='" + item.ProgramaId + "' type='button' class='btn_edit btn btn-default btn-sm'><i class='fa fa-clipboard'></i></button>";
+                resultTable += "<button data-id='" + item.ProgramaId + "'  type='button' class='btn_edit btn btn-default btn-sm'><i class='fa fa-clipboard'></i></button>";
+                resultTable += "<button data-id='" + item.ProgramaId + "' type='button' class='btn_remove btn btn-danger btn-sm'><i class='fa fa-trash-o'></i></button>";
                 resultTable += "</center></td>";
                 resultTable += "</tr>";
             });
             $('#datatable-programa').find('tbody').html(resultTable);
             $('#datatable-programa').DataTable();
+        }
+
+
+    });
+}
+function Load_Local() {
+    var data = {};
+    var _data;
+    var resultTable = "";
+    $.ajax({
+        type: "POST",
+        url: "Local/list_local",
+        data: data,
+        async: false,
+        datatype: "JSON",
+        success: function (response) {
+            console.log(response);
+            _data = JSON.parse(response);
+
+        },
+        complete: function () {
+            $.each(_data, function (i, item) {
+                resultTable += "<option value='" + item.LocalId + "'>" + item.Lc_nombre + "</option>";
+            });
+            $('#local').html(resultTable);
+        }
+
+
+    });
+}
+
+function Load_Simulacion() {
+    var data = {};
+    var _data;
+    var resultTable = "";
+    $.ajax({
+        type: "POST",
+        url: "Simulacion/list_simulacion",
+        data: data,
+        async: false,
+        datatype: "JSON",
+        success: function (response) {
+            _data = JSON.parse(response);
+
+        },
+        complete: function () {
+            $.each(_data, function (i, item) {
+                resultTable += "<option value='" + item.SimulacionId + "'>" + item.Nombre + "</option>";
+               
+            });
+            $('#simulacion').html(resultTable);
         }
 
 
